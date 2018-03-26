a61addf49f38c2d91e1106ba4b044201dc580bda
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-26 22:07:34 +0900

    tests

--- vu_meter/vu_meterTests/vu_sum_module_tests.mm
+++ vu_meter/vu_meterTests/vu_sum_module_tests.mm
@@ -31,55 +31,81 @@ using namespace yas;
     connect(module, vu::sum::input::value, 0);
     connect(module, vu::sum::output::value, 0);
 
-    {
-        time::range time_range{0, process_length};
+    auto make_stream = [sample_rate](time::range time_range, float data0, float data1) {
         proc::stream stream{sync_source{sample_rate, time_range.length}};
 
         auto &channel = stream.add_channel(0);
-        signal_event phase_signal = proc::make_signal_event<float>(process_length);
+        proc::signal_event phase_signal = proc::make_signal_event<float>(process_length);
         float *phase_data = phase_signal.data<float>();
-        phase_data[0] = 1.0f;
-        phase_data[1] = 2.0f;
+        phase_data[0] = data0;
+        phase_data[1] = data1;
         channel.insert_event(proc::time{time_range}, std::move(phase_signal));
 
+        return stream;
+    };
+
+    {
+        time::range time_range{0, process_length};
+        auto stream = make_stream(time_range, 1.0f, 2.0f);
+
         module.process(time_range, stream);
+
+        XCTAssertTrue(stream.has_channel(0));
+
+        auto const &events = stream.channel(0).events();
+
+        XCTAssertEqual(events.size(), 1);
+
+        proc::signal_event const signal = cast<proc::signal_event>(events.cbegin()->second);
+        auto const &vec = signal.vector<float>();
+
+        XCTAssertEqual(vec.size(), process_length);
+
+        XCTAssertEqual(vec.at(0), 1.0f);
+        XCTAssertEqual(vec.at(1), 3.0f);
     }
 
-    //    length_t const process_length = 6;
-    //
-    //    int16_t const left_data[3] = {
-    //        1, 2, 3,
-    //    };
-    //
-    //    int16_t const right_data[3] = {
-    //        2, 2, 2,
-    //    };
-    //
+    {
+        time::range time_range{process_length, process_length};
+        auto stream = make_stream(time_range, 3.0f, 4.0f);
 
-    //
-    //    auto module = make_signal_module<int16_t>(compare::kind::is_equal);
-    //    connect(module, compare::input::left, 0);
-    //    connect(module, compare::input::right, 1);
-    //    connect(module, compare::output::result, 2);
-    //
-    //    module.process({0, process_length}, stream);
-    //
-    //    XCTAssertTrue(stream.has_channel(2));
-    //
-    //    auto const &events = stream.channel(2).events();
-    //
-    //    XCTAssertEqual(events.size(), 1);
-    //
-    //    auto const signal = cast<signal_event>(events.cbegin()->second);
-    //    auto const &vec = signal.vector<boolean>();
-    //
-    //    XCTAssertEqual(vec.size(), process_length);
-    //    XCTAssertTrue(vec[0]);   // 0 == 0
-    //    XCTAssertFalse(vec[1]);  // 1 == 0
-    //    XCTAssertTrue(vec[2]);   // 2 == 2
-    //    XCTAssertFalse(vec[3]);  // 3 == 2
-    //    XCTAssertFalse(vec[4]);  // 0 == 2
-    //    XCTAssertTrue(vec[5]);   // 0 == 0
+        module.process(time_range, stream);
+
+        XCTAssertTrue(stream.has_channel(0));
+
+        auto const &events = stream.channel(0).events();
+
+        XCTAssertEqual(events.size(), 1);
+
+        proc::signal_event const signal = cast<proc::signal_event>(events.cbegin()->second);
+        auto const &vec = signal.vector<float>();
+
+        XCTAssertEqual(vec.size(), process_length);
+
+        XCTAssertEqual(vec.at(0), 6.0f);
+        XCTAssertEqual(vec.at(1), 10.0f);
+    }
+
+    {
+        time::range time_range{process_length * 2, process_length};
+        auto stream = make_stream(time_range, 5.0f, 6.0f);
+
+        module.process(time_range, stream);
+
+        XCTAssertTrue(stream.has_channel(0));
+
+        auto const &events = stream.channel(0).events();
+
+        XCTAssertEqual(events.size(), 1);
+
+        proc::signal_event const signal = cast<proc::signal_event>(events.cbegin()->second);
+        auto const &vec = signal.vector<float>();
+
+        XCTAssertEqual(vec.size(), process_length);
+
+        XCTAssertEqual(vec.at(0), 14.0f);
+        XCTAssertEqual(vec.at(1), 18.0f);
+    }
 }
 
 @end
