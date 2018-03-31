502d4f0aa3265eac9bca328cc6690e125a40cfc9
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-31 10:13:24 +0900

    no message

---
+++ vu_meter/vu_meter/vu_meter/RMSViewController.h
@@ -0,0 +1,14 @@
+//
+//  RMSViewController.h
+//
+
+#import <UIKit/UIKit.h>
+#import "yas_ui_metal_view_controller.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RMSViewController : YASUIMetalViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ vu_meter/vu_meter/vu_meter/RMSViewController.mm
@@ -0,0 +1,22 @@
+//
+//  RMSViewController.mm
+//
+
+#import "RMSViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RMSViewController ()
+
+@end
+
+@implementation RMSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
+
