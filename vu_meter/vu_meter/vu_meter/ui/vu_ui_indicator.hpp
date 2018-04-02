cf8922e45590f29d70272923cf7b409257ed5f4b
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-02 20:22:53 +0900

    indicator

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator.hpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator.hpp
@@ -4,6 +4,17 @@
 
 #pragma once
 
+#include "yas_ui.h"
+
 namespace yas::vu {
-struct ui_indicator {};
+struct ui_indicator {
+    ui::node node;
+    ui::rect_plane needle = ui::make_rect_plane(1);
+    std::vector<ui::rect_plane> gridlines;
+    std::vector<ui::strings> numbers;
+
+    void setup(ui::texture &texture);
+    
+    void set_value(float const);
+};
 }
--- vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator.mm
@@ -5,3 +5,11 @@
 #include "vu_ui_indicator.hpp"
 
 using namespace yas;
+
+void vu::ui_indicator::setup(ui::texture &texture) {
+#warning todo
+}
+
+void vu::ui_indicator::set_value(float const value) {
+#warning todo
+}
