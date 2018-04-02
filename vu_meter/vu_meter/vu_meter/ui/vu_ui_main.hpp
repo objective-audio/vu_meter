26cf6ba0e645bc489a134e4fdfeda9165a91601f
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-02 09:14:57 +0900

    indicators

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_main.hpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_main.hpp
@@ -6,7 +6,9 @@
 
 #include "yas_ui.h"
 #include "vu_ui_reference.hpp"
+#include "vu_ui_indicator.hpp"
 #include "vu_types.h"
+#include <array>
 
 namespace yas::vu {
 class main;
@@ -15,6 +17,7 @@ struct ui_main {
     ui::renderer renderer{ui::metal_system{make_objc_ptr(MTLCreateSystemDefaultDevice()).object()}};
 
     ui_reference reference;
+    std::array<ui_indicator, 2> indicators;
 
     void setup(main_ptr_t &main);
 };
