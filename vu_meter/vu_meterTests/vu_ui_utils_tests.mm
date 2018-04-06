4aeeaa61ab19638b54fa0e255092439fe7e1982d
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-06 22:04:13 +0900

    add ui_utils_tests

---
+++ vu_meter/vu_meterTests/vu_ui_utils_tests.mm
@@ -0,0 +1,37 @@
+//
+//  vu_ui_utils_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "vu_ui_utils.hpp"
+#import "yas_audio.h"
+
+using namespace yas;
+
+@interface vu_ui_utils_tests : XCTestCase
+
+@end
+
+@implementation vu_ui_utils_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_meter_angle {
+    XCTAssertEqualWithAccuracy(vu::ui_utils::meter_angle(audio::math::linear_from_decibel(-20.0f), 0.0f).degrees, 45.0f,
+                               0.001f);
+    XCTAssertEqualWithAccuracy(vu::ui_utils::meter_angle(audio::math::linear_from_decibel(3.0f), 0.0f).degrees, -45.0f,
+                               0.001f);
+
+    XCTAssertEqualWithAccuracy(vu::ui_utils::meter_angle(audio::math::linear_from_decibel(-40.0f), -20.0f).degrees,
+                               45.0f, 0.001f);
+    XCTAssertEqualWithAccuracy(vu::ui_utils::meter_angle(audio::math::linear_from_decibel(-17.0f), -20.0f).degrees,
+                               -45.0f, 0.001f);
+}
+
+@end
