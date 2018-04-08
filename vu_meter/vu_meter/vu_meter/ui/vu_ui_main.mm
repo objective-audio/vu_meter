c448143bcfdb8b8cae289ccf51de6740094f7527
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-08 12:46:31 +0900

    layout indicator

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
@@ -37,14 +37,20 @@ void vu::ui_main::setup(ui::renderer &&renderer, main_ptr_t &main) {
 
     // indicators
 
-    ui::layout_guide center_left_layout_guide;
-    ui::layout_guide center_right_layout_guide;
+    ui::layout_guide center_x_left_guide;
+    ui::layout_guide center_x_right_guide;
+    this->_layouts.emplace_back(ui::make_layout(
+        {.first_source_guide = safe_area_guide_rect.left(),
+         .second_source_guide = safe_area_guide_rect.right(),
+         .ratios = {100.0f, 1.0f, 100.0f},
+         .destination_guides = {ui::layout_guide{}, center_x_left_guide, center_x_right_guide, ui::layout_guide{}}}));
+
+    ui::layout_guide center_y_guide;
+
     this->_layouts.emplace_back(
-        ui::make_layout({.first_source_guide = safe_area_guide_rect.left(),
-                         .second_source_guide = safe_area_guide_rect.right(),
-                         .ratios = {100.0f, 1.0f, 100.0f},
-                         .destination_guides = {ui::layout_guide{}, center_left_layout_guide, center_right_layout_guide,
-                                                ui::layout_guide{}}}));
+        ui::make_layout({.first_source_guide = safe_area_guide_rect.top(),
+                         .second_source_guide = this->reference.layout_guide_rect.top(),
+                         .destination_guides = {ui::layout_guide{}, center_y_guide, ui::layout_guide{}}}));
 
     for (auto const &idx : {0, 1}) {
         auto &indicator = this->indicators.at(idx);
@@ -56,14 +62,31 @@ void vu::ui_main::setup(ui::renderer &&renderer, main_ptr_t &main) {
             this->_layouts.emplace_back(ui::make_layout({.source_guide = safe_area_guide_rect.left(),
                                                          .destination_guide = indicator.layout_guide_rect.left()}));
             this->_layouts.emplace_back(ui::make_layout(
-                {.source_guide = center_left_layout_guide, .destination_guide = indicator.layout_guide_rect.right()}));
+                {.source_guide = center_x_left_guide, .destination_guide = indicator.layout_guide_rect.right()}));
         } else if (idx == 1) {
             this->_layouts.emplace_back(ui::make_layout(
-                {.source_guide = center_right_layout_guide, .destination_guide = indicator.layout_guide_rect.left()}));
+                {.source_guide = center_x_right_guide, .destination_guide = indicator.layout_guide_rect.left()}));
             this->_layouts.emplace_back(ui::make_layout({.source_guide = safe_area_guide_rect.right(),
                                                          .destination_guide = indicator.layout_guide_rect.right()}));
         }
+
+        this->_layouts.emplace_back(
+            ui::layout{{.source_guides = {safe_area_guide_rect.left(), safe_area_guide_rect.right(), center_y_guide},
+                        .destination_guides = {indicator.layout_guide_rect.bottom()},
+                        .handler = [](std::vector<ui::layout_guide> const &source_guides,
+                                      std::vector<ui::layout_guide> &destination_guides) {
+                            ui::layout_guide const &src_left = source_guides.at(0);
+                            ui::layout_guide const &src_right = source_guides.at(1);
+                            ui::layout_guide const &src_y_guide = source_guides.at(2);
+                            ui::layout_guide &dst_guide = destination_guides.at(0);
+
+                            float const height = (src_right.value() - src_left.value()) * 0.5f;
+
+                            dst_guide.set_value(std::round(src_y_guide.value() - height * 0.5f));
+                        }}});
     }
+
+    // renderer observing
 
     this->_renderer_observer =
         this->renderer.subject().make_wild_card_observer([this, weak_main, texture](auto const &context) mutable {
