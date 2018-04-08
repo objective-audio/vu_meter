8c92d070e2eef0eca2601e9c56bb89f23bb15c7a
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-08 10:33:50 +0900

    ui_main layout

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
@@ -20,21 +20,58 @@ void vu::ui_main::setup(ui::renderer &&renderer, main_ptr_t &main) {
 
     this->reference.setup(main, texture);
 
-    float const offset = this->renderer.safe_area_layout_guide_rect().region().size.width / 4.0f;
+    auto const &safe_area_guide_rect = this->renderer.safe_area_layout_guide_rect();
+
+    ui::layout_guide center_layout_guide;
+    this->_layouts.emplace_back(ui::make_layout(ui::justified_layout::args{
+        .first_source_guide = safe_area_guide_rect.left(),
+        .second_source_guide = safe_area_guide_rect.right(),
+        .destination_guides = {ui::layout_guide{}, center_layout_guide, ui::layout_guide{}}}));
+
     for (auto const &idx : {0, 1}) {
         auto &indicator = this->indicators.at(idx);
         root_node.add_sub_node(indicator.node);
         indicator.setup(main, idx);
-#warning todo ちゃんとviewのサイズから算出する
-        indicator.layout(100.0f, texture);
 
-        indicator.node.set_position({.x = (idx == 0 ? -offset : offset)});
+#warning todo safe_areaの位置にバインドする
+        if (idx == 0) {
+            this->_layouts.emplace_back(ui::make_layout({.source_guide = safe_area_guide_rect.left(),
+                                                         .destination_guide = indicator.layout_guide_rect.left()}));
+            this->_layouts.emplace_back(ui::make_layout(
+                {.source_guide = center_layout_guide, .destination_guide = indicator.layout_guide_rect.right()}));
+        } else if (idx == 1) {
+            this->_layouts.emplace_back(ui::make_layout(
+                {.source_guide = center_layout_guide, .destination_guide = indicator.layout_guide_rect.left()}));
+            this->_layouts.emplace_back(ui::make_layout({.source_guide = safe_area_guide_rect.right(),
+                                                         .destination_guide = indicator.layout_guide_rect.right()}));
+        }
     }
 
-    this->_renderer_observer = this->renderer.subject().make_observer(ui::renderer::method::will_render,
-                                                                      [this, weak_main](auto const &context) {
-                                                                          for (auto &indicator : this->indicators) {
-                                                                              indicator.update();
-                                                                          }
-                                                                      });
+    this->_renderer_observer =
+        this->renderer.subject().make_wild_card_observer([this, weak_main, texture](auto const &context) mutable {
+            ui::renderer::method const &method = context.key;
+            switch (method) {
+                case ui::renderer::method::will_render: {
+                    for (auto &indicator : this->indicators) {
+                        indicator.update();
+                    }
+                } break;
+                case ui::renderer::method::view_size_changed:
+                case ui::renderer::method::safe_area_insets_changed: {
+                    ui::renderer const &renderer = context.value;
+
+                    ui::region const safe_area_region = renderer.safe_area_layout_guide_rect().region();
+                    if (safe_area_region.horizontal_range().length > 0.0 &&
+                        safe_area_region.vertical_range().length > 0.0) {
+                        float const height = safe_area_region.horizontal_range().length * 0.25f;
+                        for (auto &indicator : this->indicators) {
+                            indicator.layout(height, texture);
+                        }
+                    }
+#warning todof
+                } break;
+                default:
+                    break;
+            }
+        });
 }
