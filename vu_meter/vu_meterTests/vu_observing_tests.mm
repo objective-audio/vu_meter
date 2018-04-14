b174f177cace43e1a007145f606e8449a63148ad
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-15 08:49:49 +0900

    no message

---
+++ vu_meter/vu_meterTests/vu_observing_tests.mm
@@ -0,0 +1,39 @@
+//
+//  vu_observing_tests.m
+//  vu_meterTests
+//
+//  Created by Yuki Yasoshima on 2018/04/15.
+//  Copyright © 2018年 Yuki Yasoshima. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface vu_observing_tests : XCTestCase
+
+@end
+
+@implementation vu_observing_tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
