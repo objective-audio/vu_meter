a16fdcd6ba2043a49b1391a156ee4dff0c55b2a6
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-15 09:43:40 +0900

    no message

--- vu_meter/vu_meterTests/vu_observing_tests.mm
+++ vu_meter/vu_meterTests/vu_observing_tests.mm
@@ -25,11 +25,21 @@ using namespace yas;
 - (void)test_object {
     subject<std::string, int> subject;
 
-    subject.set_object_handler([](std::string const &key) {
-        return std::stoi(key);
-    });
+    subject.set_object_handler([](std::string const &key) { return std::stoi(key); });
 
     XCTAssertEqual(subject.object("2"), 2);
+}
+
+- (void)test_value_observer {
+    subject<std::string, int> subject;
+
+    int notified = -1;
+
+    auto observer = subject.make_value_observer("test_key", [&notified](int const &value) { notified = value; });
+
+    subject.notify("test_key", 2);
+
+    XCTAssertEqual(notified, 2);
 }
 
 @end
