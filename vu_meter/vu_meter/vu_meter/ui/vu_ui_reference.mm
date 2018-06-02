cc5dfd5aa64478fd6d9f474cd7224f92a5359f71
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-02 18:56:03 +0900

    no message

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_reference.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_reference.mm
@@ -165,6 +165,15 @@ void vu::ui_reference::_setup_flows(main_ptr_t &main) {
                                   .map([this](int32_t const &value) { return std::to_string(value) + " dB"; })
                                   .receive(this->_stepper.text_receiver())
                                   .sync());
+
+    this->_flows.emplace_back(main->data.begin_is_reference_max_flow()
+                                  .map([](bool const &value) { return !value; })
+                                  .receive(this->_stepper.plus_enabled_receiver())
+                                  .sync());
+    this->_flows.emplace_back(main->data.begin_is_reference_min_flow()
+                                  .map([](bool const &value) { return !value; })
+                                  .receive(this->_stepper.minus_enabled_receiver())
+                                  .sync());
 }
 
 ui::node &vu::ui_reference::node() {
