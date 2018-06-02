097463e2f5d79a6ec80de4f118761122911fb1d1
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-02 22:05:41 +0900

    no message

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_count.hpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_count.hpp
@@ -4,14 +4,22 @@
 
 #pragma once
 
-#include "vu_data.hpp"
-#include "vu_types.h"
+#include "vu_ui_stepper.hpp"
 #include "yas_ui.h"
 
 namespace yas::vu {
-struct ui_count {
-    ui::node node;
+class main;
 
+struct ui_count {
     void setup(main_ptr_t &main, ui::texture &texture);
+
+    ui::node &node();
+    ui::layout_guide_rect &layout_guide_rect();
+
+   private:
+    ui_stepper _stepper;
+    std::vector<flow::observer> _flows;
+
+    void _setup_flows(main_ptr_t &);
 };
 }  // namespace yas::vu
--- vu_meter/vu_meter/vu_meter/ui/vu_ui_count.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_count.mm
@@ -3,9 +3,42 @@
 //
 
 #include "vu_ui_count.hpp"
+#include "vu_data.hpp"
 #include "vu_main.hpp"
 
 using namespace yas;
 
 void vu::ui_count::setup(main_ptr_t &main, ui::texture &texture) {
+    this->_stepper.setup(texture);
+    this->_setup_flows(main);
+}
+
+void vu::ui_count::_setup_flows(main_ptr_t &main) {
+    this->_flows.emplace_back(
+        this->_stepper.begin_minus_flow().receive_null(main->data.indicator_count_decrement_receiver()).end());
+
+    this->_flows.emplace_back(
+        this->_stepper.begin_plus_flow().receive_null(main->data.indicator_count_increment_receiver()).end());
+
+    this->_flows.emplace_back(main->data.begin_indicator_count_flow()
+                                  .map([this](uint32_t const &value) { return std::to_string(value); })
+                                  .receive(this->_stepper.text_receiver())
+                                  .sync());
+    //
+    //    this->_flows.emplace_back(main->data.begin_is_reference_max_flow()
+    //                              .map([](bool const &value) { return !value; })
+    //                              .receive(this->_stepper.plus_enabled_receiver())
+    //                              .sync());
+    //    this->_flows.emplace_back(main->data.begin_is_reference_min_flow()
+    //                              .map([](bool const &value) { return !value; })
+    //                              .receive(this->_stepper.minus_enabled_receiver())
+    //                              .sync());
+}
+
+ui::node &vu::ui_count::node() {
+    return this->_stepper.node;
+}
+
+ui::layout_guide_rect &vu::ui_count::layout_guide_rect() {
+    return this->_stepper.layout_guide_rect;
 }
