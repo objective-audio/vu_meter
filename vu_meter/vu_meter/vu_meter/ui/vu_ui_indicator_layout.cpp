4aa455919f57497b4f9ec82d4766fe906dfeb4e1
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-26 22:27:32 +0900

    no message

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator_layout.cpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator_layout.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "vu_ui_indicator_layout.hpp"
+
+using namespace yas;
+using namespace yas::vu;
+
+ui_indicator_layout::ui_indicator_layout(std::size_t const count, ui::region const region)
+    : _count(count), _region(region) {
+}
--- vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator_layout.hpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_indicator_layout.hpp
@@ -3,3 +3,15 @@
 //
 
 #pragma once
+
+#include "yas_ui_types.h"
+
+namespace yas::vu {
+struct ui_indicator_layout {
+    ui_indicator_layout(std::size_t const count, ui::region const region);
+
+   private:
+    std::size_t _count;
+    ui::region _region;
+};
+}  // namespace yas::vu
