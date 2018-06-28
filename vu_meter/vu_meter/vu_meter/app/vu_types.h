1b4b72f5a1bc4084621968596cb985919ccfc394
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-29 00:08:03 +0900

    no message

--- vu_meter/vu_meter/vu_meter/app/vu_types.h
+++ vu_meter/vu_meter/vu_meter/app/vu_types.h
@@ -12,6 +12,6 @@ class main;
 using main_ptr_t = std::shared_ptr<main>;
 using weak_main_ptr_t = std::weak_ptr<main>;
 
-uint32_t constexpr indicator_count_max = 4;
+uint32_t constexpr indicator_count_max = 8;
 uint32_t constexpr indicator_count_min = 1;
 }  // namespace yas::vu
