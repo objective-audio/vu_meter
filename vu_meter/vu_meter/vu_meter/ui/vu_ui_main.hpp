6c4c42d069d17d17ef013444b6aec287935166b4
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-30 10:41:28 +0900

    no message

--- vu_meter/vu_meter/vu_meter/ui/vu_ui_main.hpp
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_main.hpp
@@ -17,8 +17,6 @@ class main;
 struct ui_main {
     ui::renderer renderer{nullptr};
 
-    ui_reference reference;
-    ui_indicator_count indicator_count;
     std::vector<ui_indicator> indicators;
 
     void setup(ui::renderer &&renderer, main_ptr_t &main);
@@ -28,14 +26,10 @@ struct ui_main {
 
     weak_main_ptr_t _weak_main;
     ui::layout_guide_rect _frame_guide_rect;
-    ui::layout_guide _vu_bottom_y_guide;
     std::vector<ui::layout_guide> _guides;
     std::vector<flow::observer> _flows;
 
     void _setup_frame_guide_rect();
-    void _setup_reference(main_ptr_t &, ui_stepper_resource &);
-    void _setup_indicator_count(main_ptr_t &, ui_stepper_resource &);
-    void _setup_vu_bottom_y_guide();
     void _setup_indicators(main_ptr_t &);
 
     void _add_indicator();
--- vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
+++ vu_meter/vu_meter/vu_meter/ui/vu_ui_main.mm
@@ -25,9 +25,6 @@ void vu::ui_main::setup(ui::renderer &&renderer, main_ptr_t &main) {
     ui_stepper_resource resource{texture};
 
     this->_setup_frame_guide_rect();
-    this->_setup_reference(main, resource);
-    this->_setup_indicator_count(main, resource);
-    this->_setup_vu_bottom_y_guide();
     this->_setup_indicators(main);
 }
 
@@ -42,110 +39,42 @@ void vu::ui_main::_setup_frame_guide_rect() {
                                   .sync());
 }
 
-void vu::ui_main::_setup_reference(main_ptr_t &main, ui_stepper_resource &resource) {
-    ui::node &root_node = this->renderer.root_node();
-
-    root_node.add_sub_node(this->reference.node());
-
-    this->reference.setup(main, resource);
-
-    this->_flows.emplace_back(this->_frame_guide_rect.begin_flow()
-                                  .map([](ui::region const &region) {
-                                      float const height = 60.0f;
-                                      bool const is_landscape = region.size.width >= region.size.height;
-                                      if (is_landscape) {
-                                          float const width = 300.0f;
-                                          return ui::region{
-                                              .origin = {.x = region.right() - width, .y = region.origin.y},
-                                              .size = {.width = width, .height = height}};
-                                      } else {
-                                          return ui::region{.origin = {.x = region.origin.x, .y = region.origin.y},
-                                                            .size = {.width = region.size.width, .height = height}};
+void vu::ui_main::_setup_indicators(main_ptr_t &main) {
+    this->_flows.emplace_back(main->indicator_count.begin_flow()
+                                  .perform([this](std::size_t const &value) {
+                                      if (value < this->indicators.size()) {
+                                          auto each = make_fast_each(this->indicators.size() - value);
+                                          while (yas_each_next(each)) {
+                                              this->_remove_indicator();
+                                          }
+                                      } else if (this->indicators.size() < value) {
+                                          auto each = make_fast_each(value - this->indicators.size());
+                                          while (yas_each_next(each)) {
+                                              this->_add_indicator();
+                                          }
                                       }
                                   })
-                                  .receive(this->reference.layout_guide_rect().receiver())
+                                  .to_tuple()
+                                  .combine(this->_frame_guide_rect.begin_flow().to_tuple())
+                                  .map([](std::tuple<std::size_t, ui::region> const &tuple) {
+                                      std::size_t const &count = std::get<0>(tuple);
+                                      ui::region const &region = std::get<1>(tuple);
+                                      return ui_indicator_layout::regions(count, region);
+                                  })
+                                  .perform([this](std::vector<ui::region> const &regions) {
+                                      std::size_t const count = std::min(this->indicators.size(), regions.size());
+                                      auto each = make_fast_each(count);
+                                      while (yas_each_next(each)) {
+                                          std::size_t const &idx = yas_each_index(each);
+                                          ui::region const &region = regions.at(idx);
+                                          this->indicators.at(idx).frame_layout_guide_rect().set_region(region);
+
+                                          if (idx == 0) {
+                                              this->_indicator_resource.set_vu_height(region.size.height);
+                                          }
+                                      }
+                                  })
                                   .sync());
-}
-
-void vu::ui_main::_setup_indicator_count(main_ptr_t &main, ui_stepper_resource &resource) {
-    ui::node &root_node = this->renderer.root_node();
-
-    root_node.add_sub_node(this->indicator_count.node());
-
-    this->indicator_count.setup(main, resource);
-
-    this->_flows.emplace_back(
-        this->_frame_guide_rect.begin_flow()
-            .combine(this->reference.layout_guide_rect().top().begin_flow().map(flow::add(vu::padding)))
-            .map([](std::pair<ui::region, float> const &pair) {
-                ui::region const &region = pair.first;
-                float const height = 60.0f;
-                bool const is_landscape = region.size.width >= region.size.height;
-                if (is_landscape) {
-                    float const width = 300.0f;
-                    return ui::region{.origin = {.x = region.origin.x, .y = region.origin.y},
-                                      .size = {.width = width, .height = height}};
-                } else {
-                    float const bottom_y = pair.second;
-                    return ui::region{.origin = {.x = region.origin.x, .y = bottom_y},
-                                      .size = {.width = region.size.width, .height = height}};
-                }
-            })
-            .receive(this->indicator_count.layout_guide_rect().receiver())
-            .sync());
-}
-
-void vu::ui_main::_setup_vu_bottom_y_guide() {
-    this->_flows.emplace_back(this->indicator_count.layout_guide_rect()
-                                  .top()
-                                  .begin_flow()
-                                  .map(flow::add(vu::padding))
-                                  .receive(this->_vu_bottom_y_guide.receiver())
-                                  .sync());
-}
-
-void vu::ui_main::_setup_indicators(main_ptr_t &main) {
-    this->_flows.emplace_back(
-        main->indicator_count.begin_flow()
-            .perform([this](std::size_t const &value) {
-                if (value < this->indicators.size()) {
-                    auto each = make_fast_each(this->indicators.size() - value);
-                    while (yas_each_next(each)) {
-                        this->_remove_indicator();
-                    }
-                } else if (this->indicators.size() < value) {
-                    auto each = make_fast_each(value - this->indicators.size());
-                    while (yas_each_next(each)) {
-                        this->_add_indicator();
-                    }
-                }
-            })
-            .to_tuple()
-            .combine(this->_frame_guide_rect.begin_flow().to_tuple())
-            .combine(this->_vu_bottom_y_guide.begin_flow().to_tuple())
-            .map([](std::tuple<std::size_t, ui::region, float> const &tuple) {
-                std::size_t const &count = std::get<0>(tuple);
-                ui::region const &region = std::get<1>(tuple);
-                float const &bottom_y = std::get<2>(tuple);
-
-                return ui_indicator_layout::regions(
-                    count, ui::region{.origin = {.x = region.left(), .y = bottom_y},
-                                      .size = {.width = region.size.width, .height = region.top() - bottom_y}});
-            })
-            .perform([this](std::vector<ui::region> const &regions) {
-                std::size_t const count = std::min(this->indicators.size(), regions.size());
-                auto each = make_fast_each(count);
-                while (yas_each_next(each)) {
-                    std::size_t const &idx = yas_each_index(each);
-                    ui::region const &region = regions.at(idx);
-                    this->indicators.at(idx).frame_layout_guide_rect().set_region(region);
-
-                    if (idx == 0) {
-                        this->_indicator_resource.set_vu_height(region.size.height);
-                    }
-                }
-            })
-            .sync());
 }
 
 void vu::ui_main::_add_indicator() {
