b891c574a1ec805775c8b142abedbe4517374983
Yuki Yasoshima <yukiyasos@gmail.com>
2021-08-06 23:13:50 +0900

    data

--- vu_meter/vu_meter/vu_meter/app/vu_data.hpp
+++ vu_meter/vu_meter/vu_meter/app/vu_data.hpp
@@ -12,13 +12,13 @@ class data;
 using data_ptr = std::shared_ptr<data>;
 
 struct data {
-    struct impl;
-
     int32_t reference() const;
 
     static data_ptr make_shared();
 
    private:
+    struct impl;
+
     std::unique_ptr<impl> _impl;
 
     data();
