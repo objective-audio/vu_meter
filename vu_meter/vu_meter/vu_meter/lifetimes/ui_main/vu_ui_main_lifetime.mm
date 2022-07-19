75bde7fa0bf2443bb579a1f0a6757fb0659ad15e
Yuki Yasoshima <yukiyasos@gmail.com>
2022-07-19 22:23:17 +0900

    factory resource

--- vu_meter/vu_meter/vu_meter/lifetimes/ui_main/vu_ui_main_lifetime.mm
+++ vu_meter/vu_meter/vu_meter/lifetimes/ui_main/vu_ui_main_lifetime.mm
@@ -5,6 +5,8 @@
 #include "vu_ui_main_lifetime.hpp"
 #include "vu_ui_background.hpp"
 #include "vu_ui_indicator_container.hpp"
+#include "vu_ui_indicator_factory.hpp"
+#include "vu_ui_indicator_resource.hpp"
 
 using namespace yas;
 using namespace yas::vu;
@@ -14,5 +16,8 @@ std::shared_ptr<ui_main_lifetime> ui_main_lifetime::make_shared() {
 }
 
 ui_main_lifetime::ui_main_lifetime()
-    : background(ui_background::make_shared()), indicator_container(ui_indicator_container::make_shared()) {
+    : background(ui_background::make_shared()),
+      indicator_resource(ui_indicator_resource::make_shared()),
+      indicator_factory(ui_indicator_factory::make_shared(this->indicator_resource)),
+      indicator_container(ui_indicator_container::make_shared()) {
 }
